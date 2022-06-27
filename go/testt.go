e2ddf846c205bf403b33cd8ef743626febc2fcd8
martvburg <40295159+martvburg@users.noreply.github.com>
2022-06-27 17:21:50 +0200

    Update testt.go

--- go/testt.go
+++ go/testt.go
@@ -8,12 +8,17 @@ import (
 
 var wg sync.WaitGroup
 
+func foo(){
+	defer fmt.Println("second")
+	fmt.Println("first")
+}
+
 func say(s string) {
+	defer wg.Done()
 	for i:=0; i < 3; i++ {
 		time.Sleep(100*time.Millisecond)
 		fmt.Println(s)
 	}	
-	wg.Done()
 }
 
 func main() {
@@ -21,5 +26,7 @@ func main() {
 	go say("Hey")
 	wg.Add(1)
 	go say("there")
+	wg.Add(1)
+	go say("hi")
 	wg.Wait()
 }
